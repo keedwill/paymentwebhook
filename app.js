2e8530a262a893a41b4b836d262a1020e02c9e18
owoh princewill <princewillowoh@gmail.com>
2025-04-18 15:40:17 +0100

    commit

--- app.js
+++ app.js
@@ -1,44 +1,97 @@
-var createError = require('http-errors');
+// var createError = require('http-errors');
 var express = require('express');
 const bodyParser = require("body-parser");
-var path = require('path');
-var cookieParser = require('cookie-parser');
+// var path = require('path');
+// var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
 
 var app = express();
 const PORT = process.env.PORT || 7000;
 app.use(bodyParser.json());
+
+
+app.post("/webhook/paystack", async (req, res) => {
+  console.log("Received webhook from Paystack:", req.body);
+  res.status(200);
+  // const event = req.body.event;
+  // const data = req.body.data;
+
+  // if (event === "charge.success" || event === "transfer.success") {
+  //   console.log("Payment successful:", data);
+
+  //   const customerEmail = data.customer.email;
+  //   const paymentData = {
+  //     reference: data.reference,
+  //     amount: data.amount,
+  //     paid_at: data.paid_at,
+  //     paymentStatus: data.status,
+  //     firstName: data.customer.first_name,
+  //     lastName: data.customer.last_name,
+  //     email: customerEmail,
+  //     currency: data.currency,
+  //   };
+
+  //   // Generate the barcode image URL
+  //   const barcodeImageUrl = await generateBarcode(paymentData);
+
+  //   if (barcodeImageUrl) {
+  //     // Send the receipt email
+  //     const emailSent = await sendReceiptEmail(
+  //       customerEmail,
+  //       paymentData,
+  //       barcodeImageUrl
+  //     );
+  //     if (emailSent) {
+  //       return res
+  //         .status(200)
+  //         .send("Webhook received and email sent successfully.");
+  //     } else {
+  //       return res
+  //         .status(500)
+  //         .send("Webhook received, but failed to send email.");
+  //     }
+  //   } else {
+  //     return res
+  //       .status(500)
+  //       .send("Webhook received, but failed to generate barcode.");
+  //   }
+  // } else {
+  //   // Acknowledge other events if you're handling them
+  //   console.log("Received webhook event:", event);
+  //   return res.status(200).send("Webhook received.");
+  // }
+});
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
--- routes/index.js
+++ routes/index.js
@@ -13,57 +13,57 @@ router.get("/contact", function (req, res, next) {
   res.render("contact", { title: "Contact" });
 });
 
-router.post("/webhook/paystack", async (req, res) => {
+// router.post("/webhook/paystack", async (req, res) => {
 
-  console.log("Received webhook from Paystack:", req.body);
-   res.status(200)
-  // const event = req.body.event;
-  // const data = req.body.data;
+//   console.log("Received webhook from Paystack:", req.body);
+//    res.status(200)
+//   // const event = req.body.event;
+//   // const data = req.body.data;
 
-  // if (event === "charge.success" || event === "transfer.success") {
-  //   console.log("Payment successful:", data);
+//   // if (event === "charge.success" || event === "transfer.success") {
+//   //   console.log("Payment successful:", data);
 
-  //   const customerEmail = data.customer.email;
-  //   const paymentData = {
-  //     reference: data.reference,
-  //     amount: data.amount,
-  //     paid_at: data.paid_at,
-  //     paymentStatus: data.status,
-  //     firstName: data.customer.first_name,
-  //     lastName: data.customer.last_name,
-  //     email: customerEmail,
-  //     currency: data.currency,
-  //   };
+//   //   const customerEmail = data.customer.email;
+//   //   const paymentData = {
+//   //     reference: data.reference,
+//   //     amount: data.amount,
+//   //     paid_at: data.paid_at,
+//   //     paymentStatus: data.status,
+//   //     firstName: data.customer.first_name,
+//   //     lastName: data.customer.last_name,
+//   //     email: customerEmail,
+//   //     currency: data.currency,
+//   //   };
 
-  //   // Generate the barcode image URL
-  //   const barcodeImageUrl = await generateBarcode(paymentData);
+//   //   // Generate the barcode image URL
+//   //   const barcodeImageUrl = await generateBarcode(paymentData);
 
-  //   if (barcodeImageUrl) {
-  //     // Send the receipt email
-  //     const emailSent = await sendReceiptEmail(
-  //       customerEmail,
-  //       paymentData,
-  //       barcodeImageUrl
-  //     );
-  //     if (emailSent) {
-  //       return res
-  //         .status(200)
-  //         .send("Webhook received and email sent successfully.");
-  //     } else {
-  //       return res
-  //         .status(500)
-  //         .send("Webhook received, but failed to send email.");
-  //     }
-  //   } else {
-  //     return res
-  //       .status(500)
-  //       .send("Webhook received, but failed to generate barcode.");
-  //   }
-  // } else {
-  //   // Acknowledge other events if you're handling them
-  //   console.log("Received webhook event:", event);
-  //   return res.status(200).send("Webhook received.");
-  // }
-});
+//   //   if (barcodeImageUrl) {
+//   //     // Send the receipt email
+//   //     const emailSent = await sendReceiptEmail(
+//   //       customerEmail,
+//   //       paymentData,
+//   //       barcodeImageUrl
+//   //     );
+//   //     if (emailSent) {
+//   //       return res
+//   //         .status(200)
+//   //         .send("Webhook received and email sent successfully.");
+//   //     } else {
+//   //       return res
+//   //         .status(500)
+//   //         .send("Webhook received, but failed to send email.");
+//   //     }
+//   //   } else {
+//   //     return res
+//   //       .status(500)
+//   //       .send("Webhook received, but failed to generate barcode.");
+//   //   }
+//   // } else {
+//   //   // Acknowledge other events if you're handling them
+//   //   console.log("Received webhook event:", event);
+//   //   return res.status(200).send("Webhook received.");
+//   // }
+// });
 
 module.exports = router;
