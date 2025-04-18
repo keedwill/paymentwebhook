d39cfa109b1393a19016c7fd10e3673e4380f783
owoh princewill <princewillowoh@gmail.com>
2025-04-18 16:08:22 +0100

    commit

--- app.js
+++ app.js
@@ -14,56 +14,56 @@ app.use(bodyParser.json());
 app.use(logger("dev"));
 
 app.post("/webhook/paystack", async (req, res) => {
-  console.log("Received webhook from Paystack:", req.body);
-  res.status(200).json({ status: "healthy webhook" });
-  // res.sendStatus(200);
-  // const event = req.body.event;
-  // const data = req.body.data;
+  // console.log("Received webhook from Paystack:", req.body);
+  // res.status(200).json({ status: "healthy webhook" });
+ 
+  const event = req.body.event;
+  const data = req.body.data;
 
-  // if (event === "charge.success" || event === "transfer.success") {
-  //   console.log("Payment successful:", data);
+  if (event === "charge.success" || event === "transfer.success") {
+    console.log("Payment successful:", data);
 
-  //   const customerEmail = data.customer.email;
-  //   const paymentData = {
-  //     reference: data.reference,
-  //     amount: data.amount,
-  //     paid_at: data.paid_at,
-  //     paymentStatus: data.status,
-  //     firstName: data.customer.first_name,
-  //     lastName: data.customer.last_name,
-  //     email: customerEmail,
-  //     currency: data.currency,
-  //   };
+    const customerEmail = data.customer.email;
+    const paymentData = {
+      reference: data.reference,
+      amount: data.amount,
+      paid_at: data.paid_at,
+      paymentStatus: data.status,
+      firstName: data.customer.first_name,
+      lastName: data.customer.last_name,
+      email: customerEmail,
+      currency: data.currency,
+    };
 
-  //   // Generate the barcode image URL
-  //   const barcodeImageUrl = await generateBarcode(paymentData);
+    // Generate the barcode image URL
+    const barcodeImageUrl = await generateBarcode(paymentData);
 
-  //   if (barcodeImageUrl) {
-  //     // Send the receipt email
-  //     const emailSent = await sendReceiptEmail(
-  //       customerEmail,
-  //       paymentData,
-  //       barcodeImageUrl
-  //     );
-  //     if (emailSent) {
-  //       return res
-  //         .status(200)
-  //         .send("Webhook received and email sent successfully.");
-  //     } else {
-  //       return res
-  //         .status(500)
-  //         .send("Webhook received, but failed to send email.");
-  //     }
-  //   } else {
-  //     return res
-  //       .status(500)
-  //       .send("Webhook received, but failed to generate barcode.");
-  //   }
-  // } else {
-  //   // Acknowledge other events if you're handling them
-  //   console.log("Received webhook event:", event);
-  //   return res.status(200).send("Webhook received.");
-  // }
+    if (barcodeImageUrl) {
+      // Send the receipt email
+      const emailSent = await sendReceiptEmail(
+        customerEmail,
+        paymentData,
+        barcodeImageUrl
+      );
+      if (emailSent) {
+        return res
+          .status(200)
+          .json("Webhook received and email sent successfully.");
+      } else {
+        return res
+          .status(500)
+          .json("Webhook received, but failed to send email.");
+      }
+    } else {
+      return res
+        .status(500)
+        .json("Webhook received, but failed to generate barcode.");
+    }
+  } else {
+    // Acknowledge other events if you're handling them
+    console.log("Received webhook event:", event);
+    return res.status(200).json("Webhook received.");
+  }
 });
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
